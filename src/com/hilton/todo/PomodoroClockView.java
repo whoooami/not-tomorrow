2561d7b8fbf1f0ff8a015f45dcfc3529773ca8d1
dragonhilton <dragonhilton@gmail.com>
2013-05-07 14:19:24 +0000

    Make pomodoro clock count down.

--- src/com/hilton/todo/PomodoroClockView.java
+++ src/com/hilton/todo/PomodoroClockView.java
@@ -3,25 +3,94 @@ package com.hilton.todo;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class PomodoroClockView extends View {
+    private static final String TAG = "PomodoroClockView";
+    private int mViewWidth;
+    private int mViewHeight;
+    private int mClockRadius;
+    private RectF mWorkingArea;
+    private RectF mRestArea;
+    private RectF mStatusPanel;
+    private Paint mRestPaint;
+    private Paint mWorkingPaint;
+    private Paint mCountDownPaint;
+    private float mSweepAngle;
+    private float mRemainingTime;
+    
     public PomodoroClockView(Context context) {
 	this(context, null);
-    }
-
-    public PomodoroClockView(Context context, AttributeSet attrs, int defStyle) {
-	super(context, attrs, defStyle);
     }
 
     public PomodoroClockView(Context context, AttributeSet attrs) {
 	this(context, attrs, 0);
     }
+    
+    public PomodoroClockView(Context context, AttributeSet attrs, int defStyle) {
+	super(context, attrs, defStyle);
+	mRestArea = new RectF();
+	mWorkingArea = new RectF();
+	
+	mRestPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	mRestPaint.setColor(Color.GREEN);
+	mRestPaint.setStyle(Style.FILL_AND_STROKE);
+	
+	mWorkingPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	mWorkingPaint.setColor(Color.RED);
+	mWorkingPaint.setStyle(Style.FILL_AND_STROKE);
+	
+	mCountDownPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	mCountDownPaint.setColor(Color.GRAY);
+	mCountDownPaint.setAlpha(200);
+	mCountDownPaint.setStyle(Style.FILL);
+	mSweepAngle = 0.0f;
+	mRemainingTime = 1800.0f; // in seconds; 30 mins = 30 * 60 = 1800 s
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+	super.onSizeChanged(w, h, oldw, oldh);
+	mViewHeight = h;
+	mViewWidth = w;
+	mClockRadius = h / 4;
+	final float top = 2.0f * h / 20.0f;
+	final float left = w/2.0f - mClockRadius;
+	mRestArea.top = top;
+	mRestArea.left = left;
+	mRestArea.right = left + mClockRadius;
+	mRestArea.bottom = top + mClockRadius;
+	
+	mWorkingArea.top = top;
+	mWorkingArea.left = left;
+	mWorkingArea.bottom = top + 2 * mClockRadius;
+	mWorkingArea.right = left + 2 * mClockRadius;
+	Log.e(TAG, "onsize changed ");
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+	super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
 	canvas.drawColor(Color.GRAY);
+	canvas.drawArc(mWorkingArea, 210, 60, true, mRestPaint);
+	canvas.drawArc(mWorkingArea, 270, 300, true, mWorkingPaint);
+	canvas.drawArc(mWorkingArea, 270, mSweepAngle, true, mCountDownPaint);
 	super.onDraw(canvas);
+	if (mRemainingTime <= 0.0f) {
+	    // destroy activity
+	    return;
+	}
+	mRemainingTime -= 5.0f;
+	mSweepAngle += 1.0f;
+	postInvalidateDelayed(500, (int) mWorkingArea.left, (int) mWorkingArea.top, (int) mWorkingArea.right, (int) mWorkingArea.bottom);
     }
 }
