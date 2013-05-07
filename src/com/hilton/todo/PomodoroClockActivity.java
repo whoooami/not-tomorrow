a53ca4e37159e6aa81e86a2eee39838134a1e449
dragonhilton <dragonhilton@gmail.com>
2013-05-07 15:36:23 +0000

    Show obviously digital clock status  to user.
    Clock view is emotionally. clock status is rationally. When clock status counting down to 0 we close the Pomodoro.

--- src/com/hilton/todo/PomodoroClockActivity.java
+++ src/com/hilton/todo/PomodoroClockActivity.java
@@ -4,17 +4,37 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.hilton.todo.PomodoroClockView.OnTimeoutListener;
 import com.hilton.todo.TaskStore.TaskColumns;
 
 public class PomodoroClockActivity extends Activity {
+    private static final int MSG_TERMINATE = 10;
+    private static final int MSG_REFRESH_CLOCK = 0;
     private int mInterruptsCount;
     private int mSpentPomodoros;
-    
+    private int mRemainingTimeInSeconds;
+    private Handler mHandler = new Handler() {
+	@Override
+	public void handleMessage(Message msg) {
+	    switch (msg.what) {
+	    case MSG_TERMINATE:
+		finish();
+		break;
+	    case MSG_REFRESH_CLOCK:
+		mRemainingTimeInSeconds--;
+		updateClockStatus();
+		break;
+	    default:
+		break;
+	    }
+	}
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
@@ -50,12 +70,38 @@ public class PomodoroClockActivity extends Activity {
 	    }
 	});
 	
-	final PomodoroClockView clock = (PomodoroClockView) findViewById(R.id.clock_view);
-	clock.setOnTimeoutListener(new OnTimeoutListener() {
-	    @Override
-	    public void onTimeout() {
-		finish();
-	    }
-	});
+	mRemainingTimeInSeconds = 180; // 30 mins
+	updateClockStatus();
+    }
+
+    private void updateClockStatus() {
+	final TextView clockStatus = (TextView) findViewById(R.id.clock_status);
+	String status = "";
+	if (mRemainingTimeInSeconds <= 0) {
+	    status = "Time is up!";
+	    clockStatus.setText(status);
+	    mHandler.removeMessages(MSG_REFRESH_CLOCK);
+	    mHandler.removeMessages(MSG_TERMINATE);
+	    mHandler.sendEmptyMessageDelayed(MSG_TERMINATE, 1000);
+	    return;
+	}
+	int min = mRemainingTimeInSeconds / 60;
+	int sec = mRemainingTimeInSeconds % 60;
+	if (mRemainingTimeInSeconds >= 300) {
+	    status = "Work time:   -";
+	    min = (mRemainingTimeInSeconds - 300) / 60;
+	    sec = (mRemainingTimeInSeconds - 300) % 60;
+	} else {
+	    status = "Rest time:  -";
+	}
+	status += min;
+	status += ":";
+	if (sec < 10) {
+	    status += "0";
+	}
+	status += sec;
+	clockStatus.setText(status);
+	mHandler.removeMessages(MSG_REFRESH_CLOCK);
+	mHandler.sendEmptyMessageDelayed(MSG_REFRESH_CLOCK, 1000);
     }
 }
