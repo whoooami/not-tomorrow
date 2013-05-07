9aa2e298799dbc992307af9052741520cf3f04e8
dragonhilton@gmail.com <dragonhilton@gmail.com@bb396b09-ee00-a5ed-754c-254848320f1c>
2013-05-07 10:52:57 +0000

    Show task description in pomodoro clock.

--- trunk/src/com/hilton/todo/PomodoroClockActivity.java
+++ trunk/src/com/hilton/todo/PomodoroClockActivity.java
@@ -2,11 +2,15 @@ package com.hilton.todo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PomodoroClockActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
 	setContentView(R.layout.pomodoro_clock);
+	
+	final TextView taskDescription = (TextView) findViewById(R.id.status_panel);
+	taskDescription.setText(getIntent().getStringExtra(TaskDetailsActivity.EXTRA_TASK_CONTENT));
     }
 }
